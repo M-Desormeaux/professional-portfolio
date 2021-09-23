583700d6682d13c6582a8b3e7d0bb3ee62d428d3
Michael <michael.desormeaux@scswla.org>
2021-09-23 09:30:00 -0500

    scrap: scrapping original concept and starting from scratch when i get time

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { AppWidth, GlobalStyle } from "./AppStyle";
-import { Landing } from "./components/Landing/Landing";
 
 export const App = () => {
   return (
     <>
-      <AppWidth>
-        <GlobalStyle />
-        <Landing />
-        <div style={{ background: "blue", height: "100px" }}></div>
-      </AppWidth>
+      <h1>Hi there!</h1>
     </>
   );
 };
